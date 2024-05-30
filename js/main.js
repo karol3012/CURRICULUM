5ff6d9f116d7efb9c7bedf8439c9a106c63f936f
karol3012 <kjarevalope@ufpso.edu.co>
2024-05-30 00:45:20 -0500

    load modified funtions main js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ document.querySelectorAll(".hamburguer")[0].addEventListener("click", function()
         document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
         ico_hamburguer= false;
     }else{
-        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
+        document.querySelectorAll(".hamburguer")[0].style.color ="#2b9ed8";
         ico_hamburguer= true;
     }
 
@@ -17,3 +17,18 @@ document.querySelectorAll(".enlaces-header")[0].addEventListener("click", functi
     enlaces_header.classList.toggle("hamburgueropen");
     document.querySelectorAll(".hamburguer")[0].style.color ="#000";
 })
+
+
+
+// Funcion para colocar una transición mas lenta a los hipervinculos del nav
+
+const links  = document.querySelectorAll('.link');
+
+links.forEach( (link) => {
+    link.addEventListener('click', (e) =>{
+        e.preventDefault();
+        const element = document.querySelector(link.getAttribute('href'));
+
+        element.scrollIntoView({block: "start", behavior: "smooth"});
+    });
+});
